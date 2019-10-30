5958f4392d0ae396c9b3d06d2aff6dc5c6d8dba6
Liu Zechu <liuzechu2013@gmail.com>
2019-10-30 19:03:31 +0800

    add viewplan to parser

--- src/main/java/seedu/address/logic/parser/ModulePlannerParser.java
+++ src/main/java/seedu/address/logic/parser/ModulePlannerParser.java
@@ -42,6 +42,7 @@ import seedu.address.logic.commands.storage.ListAllStudyPlansCommand;
 import seedu.address.logic.commands.storage.RevertCommitCommand;
 import seedu.address.logic.commands.storage.ViewCommitCommand;
 import seedu.address.logic.commands.storage.ViewCommitHistoryCommand;
+import seedu.address.logic.commands.storage.ViewStudyPlanCommand;
 import seedu.address.logic.commands.verification.CheckCommand;
 import seedu.address.logic.commands.verification.DescriptionCommand;
 import seedu.address.logic.commands.verification.ValidModsCommand;
@@ -72,6 +73,7 @@ import seedu.address.logic.parser.storage.EditStudyPlanTitleParser;
 import seedu.address.logic.parser.storage.ListAllStudyPlansParser;
 import seedu.address.logic.parser.storage.RevertCommitParser;
 import seedu.address.logic.parser.storage.ViewCommitCommandParser;
+import seedu.address.logic.parser.storage.ViewStudyPlanCommandParser;
 import seedu.address.logic.parser.verification.DescriptionCommandParser;
 import seedu.address.logic.parser.verification.ValidModsCommandParser;
 
@@ -216,6 +218,9 @@ public class ModulePlannerParser {
 
         case TagStudyPlanCommand.COMMAND_WORD:
             return new TagStudyPlanCommandParser().parse(arguments);
+
+        case ViewStudyPlanCommand.COMMAND_WORD:
+            return new ViewStudyPlanCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
